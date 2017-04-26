0ba4cce8cc9d058612128582bad9e6784af3f9b7
Jenei Máté <jmate0321@gmail.com>
2017-04-26 22:52:41 +0200

    fixed csmacnz.Coveralls params

--- code-coverage.bat
+++ code-coverage.bat
@@ -1,2 +1,3 @@
-packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" "-targetargs:""UnitTests\bin\Debug\UnitTests.dll"" " -output:opencoverCoverage.xml
-packages\coveralls.net.0.8.0-unstable0013\tools\csmacnz.Coveralls --opencover -i opencoverCoverage.xml --repoToken TKOBT2qfpbd23Mrff76hEwBgYKoy9eHOZ
+packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -coverbytest:*Tests.dll -log:Verbose -target:"packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" "-targetargs:""UnitTests\bin\Debug\UnitTests.dll"" " -output:opencoverCoverage.xml
+packages\coveralls.net.0.8.0-unstable0013\tools\csmacnz.Coveralls --opencover -i opencoverCoverage.xml --repoToken TKOBT2qfpbd23Mrff76hEwBgYKoy9eHOZ --useRelativePaths --commitBranch master
+pause
