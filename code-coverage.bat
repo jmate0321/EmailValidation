e21f83ca287e1d0d247f92e27e9433b85f3c0df2
Jenei Máté <jmate0321@gmail.com>
2017-04-26 23:01:03 +0200

    added filters to OpenCov

--- code-coverage.bat
+++ code-coverage.bat
@@ -1,3 +1,2 @@
-packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -coverbytest:*Tests.dll -log:Verbose -target:"packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" "-targetargs:""UnitTests\bin\Debug\UnitTests.dll"" " -output:opencoverCoverage.xml
+packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -coverbytest:*Tests.dll -log:Verbose -target:"packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" "-targetargs:""UnitTests\bin\Debug\UnitTests.dll"" " -output:opencoverCoverage.xml -filter:"+[*]* -[*DataAnnotations*]*"
 packages\coveralls.net.0.8.0-unstable0013\tools\csmacnz.Coveralls --opencover -i opencoverCoverage.xml --repoToken TKOBT2qfpbd23Mrff76hEwBgYKoy9eHOZ --useRelativePaths --commitBranch master
-pause
